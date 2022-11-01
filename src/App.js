fb8a01d05e2d8bdd8949305888ee4a3c6a79bcda
Esteban Pajares <jehupajares@gmail.com>
2022-11-01 01:58:26 -0500

    Definiendo las secciones del proyecto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import logo from './logo.svg';
-import styled from 'styled-components';
-
 import './App.css';
+import Layout from './components/layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      {/* Creando los componentes */}
+      <h1>Starting GitList</h1>
+      <div>
+        profile
+      </div>
+      <div>
+        filters
+      </div>
+      <div>
+        repoList
+      </div>
+      <div>
+        search
+      </div>
+    </Layout>
   );
 }
 
---
+++ src/components/layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const LayoutStyled = styled.main`
+
+`
+
+function Layout( {children} ) {
+    return (
+        <LayoutStyled>
+            {children}
+        </LayoutStyled>
+    )
+}
+
+export default Layout
